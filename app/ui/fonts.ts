e2fcae4da3e595cb02c46f87c44fde51f90b3775
ox1212 <simoghof@gmail.com>
2023-10-27 21:30:27 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Space_Mono } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const spacemono = Space_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
